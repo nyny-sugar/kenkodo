0a2906c5f90372b8c1019c8a9d841d5d07e77aa5
natsu <nyny.sugar@gmail.com>
2022-06-22 19:40:24 +0900

    環境構築、header作成

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    backgroundImage: {
+      "top-bg": "url('../src/img/top.jpg')",
+    },
+  },
+  plugins: [],
+};
