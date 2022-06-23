aeea71b1cdaa073cd0cac50fe890b372b09298a4
natsu <nyny.sugar@gmail.com>
2022-06-23 19:27:52 +0900

    top作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,31 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    spacing: {
+      1: "8px",
+      2: "12px",
+      3: "16px",
+      4: "24px",
+      5: "32px",
+      6: "48px",
+    },
+    colors: {
+      brown: "#78635c",
+      yellow: "#dfb63b",
+    },
     extend: {},
     backgroundImage: {
       "top-bg": "url('../src/img/top.jpg')",
+    },
+    aspectRatio: {
+      "4/3": "4 / 3",
+      "1/1": "1 / 1",
     },
   },
   plugins: [],
